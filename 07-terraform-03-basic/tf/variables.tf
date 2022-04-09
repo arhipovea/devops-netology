519c9a3ecb31b1590da6b2086976e0df998b9c63
Arhipov Evgeniy <arhipovea@google.com>
2022-04-09 22:57:05 +0300

    7.3

---
+++ 07-terraform-03-basic/tf/variables.tf
@@ -0,0 +1,40 @@
+variable "yc_token" {
+  type = string
+  description = "Yandex Cloud OAuth token"
+}
+
+variable "yc_cloud_id" {
+  type = string
+  description = "Yandex Cloud ID"
+  default = "b1gdn3k0lg5v8o3qnp4j"
+}
+
+variable "yc_folder_id" {
+  type = string
+  description = "Yandex Cloud Folder ID"
+  default = "b1g68ge17ldu86fka0mt"
+}
+
+variable "ubuntu_family" {
+  type = string
+  description = "Ubuntu family"
+  default = "ubuntu-2004-lts"
+}
+
+variable "platform" {
+  type        = map
+  default     = {
+    "prod" = "standard-v1",
+    "stage" = "standard-v3"
+  }
+}
+
+variable "instance_count" {
+  type        = map
+  default     = {
+    "prod" = 2,
+    "stage" = 1
+  }
+}
+
+
