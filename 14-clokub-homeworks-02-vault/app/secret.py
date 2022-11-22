dd891917a05af830d8b27ad0b2a0c710098b833a
Arhipov Evgeniy <arhipovea@gmail.com>
2022-11-22 15:55:51 +0300

    fix 14.2, add k8s secrets

--- 14-clokub-homeworks-02-vault/app/secret.py
+++ 14-clokub-homeworks-02-vault/app/secret.py
@@ -9,13 +9,10 @@ client = hvac.Client(url=VAULT_URL, token=VAULT_TOKEN)
 client.is_authenticated()
 
 # Пишем секрет
-client.secrets.kv.v2.create_or_update_secret(path="hvac", secret=dict(netology22="Big secret!!! Second exercise v2"))
+client.secrets.kv.v2.create_or_update_secret(path="hvac", secret=dict(netology="Big secret!!! Second exercise v3"))
 
 # Читаем секрет
 secret_response = client.secrets.kv.v2.read_secret_version(path="hvac")
-
-with open("prog_result", "w") as f:
-    f.write(f"Secret keys: {secret_response['data']['data'].keys()}")
 
 while True:
     pass
